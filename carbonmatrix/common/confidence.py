9d0d1499b503eec97d096bca74f9da8e38d14a98
Fredo Guan <fredo.guan@hotmail.com>
2026-02-20 20:36:32 -0800

    Update confidence.py
    add pair iptm

--- carbonmatrix/common/confidence.py
+++ carbonmatrix/common/confidence.py
@@ -35,6 +35,41 @@ def compute_ptm(logits, breaks, mask, chain_id=None, interface=True):
 
     return ptm
 
+def compute_pair_iptm(logits, breaks, mask, chain_id):
+    bin_centers = _calculate_bin_centers(breaks) # (bins,)
+
+    num_res = torch.sum(mask, dim=-1, keepdims=True) #(bs, 1)
+    clipped_num_res = torch.clip(num_res, 19, 100000)
+
+    d0 = 1.24 * (clipped_num_res - 15) ** (1.0 / 3) - 1.8
+
+    probs = F.softmax(logits, dim=-1) #(bs, n, n, d)
+    
+    tm_per_bin = 1.0 / (1 + (bin_centers ** 2) / (d0 ** 2)) # (bs, bins)
+    tm_per_bin = rearrange(tm_per_bin, 'b d -> b () () d') # (bs, 1, 1, bins)
+    predicted_tm_term = torch.sum(probs * tm_per_bin, dim=-1) # (bs, n, n)
+
+    chain_pair_iptm = {}
+    chain_id_list = torch.unique(chain_id).tolist()
+    for idx1 in asym_ids_list:
+        chain_iptm = {}
+        for idx2 in asym_ids_list:
+            mask_pair_chain = (
+                (chain_id[:, None, :] == idx1)
+                * (chain_id[:, :, None] == idx2)
+                * mask[:, None, :]
+                * mask[:, :, None]
+            )
+
+            chain_iptm[idx2] = torch.max(
+                torch.sum(predicted_tm_term * mask_pair_chain, dim=-1)
+                / (torch.sum(mask_pair_chain, dim=-1) + 1e-5),
+                dim=1,
+            ).values
+        chain_pair_iptm[idx1] = chain_iptm
+
+    return chain_pair_iptm
+
 def _calculate_bin_centers(boundaries: torch.Tensor):
     step = boundaries[1] - boundaries[0]
     bin_centers = boundaries + step / 2
